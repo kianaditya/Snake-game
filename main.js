9a1bab371e4235ade4044e32f233c9d4d248febf
Aditya Naik <naik.aditya@gmail.com>
2018-10-25 12:42:54 +0200

    Added head evaluator to snake body hash to keep track of head of snake

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ class Snake {
         this.snake_body = snake_body;
     }
     
-    static drawSnakeUnit (ctx,x,y,head =false){
-
-        ctx.fillStyle = head ? "red" : "gray";
+    static drawSnakeUnit (ctx,x,y,head){
+        
+        ctx.fillStyle = head === 'Yes' ? "red" : "gray";
         ctx.fillRect(x,y,5,5);
 
     }
@@ -20,10 +20,11 @@ class Snake {
     }
 
     static startSnake(length){
+        
         let x_position = Math.floor(Math.random()*(parseInt(myCanvas.width,10)/2-101)+100);
         let y_position = Math.floor(Math.random()*(parseInt(myCanvas.height,10)/2-81)+80);
         let init_direction = direction[Math.floor(Math.random()*4)];
-        let snake_skeleton = [{x: x_position,y: y_position,direction:init_direction}];
+        let snake_skeleton = [{x: x_position,y: y_position,direction:init_direction,head:'Yes'}];
 
         for (let i = 0 ; i < length-1 ; i++){
             x_position += init_direction == 'left' ? 5 : 0;
@@ -31,22 +32,18 @@ class Snake {
             y_position += init_direction == "up" ? 5 : 0 ;
             y_position -= init_direction == "down" ? 5:0;
 
-            snake_skeleton.push({x: x_position,y: y_position,direction:init_direction});
+            snake_skeleton.push({x: x_position,y: y_position,direction:init_direction,head:'No'});
 
         }
-        console.log(snake_skeleton);
+
         return snake_skeleton;
     }
 
     static drawSnake(snake_skeleton){
 
-        let head = true;
-
         for(let i = 0; i< snake_skeleton.length;i++){
 
-            this.drawSnakeUnit(ctx,Object.values(snake_skeleton[i])[0],Object.values(snake_skeleton[i])[1],head);
-            head = false;
-
+            this.drawSnakeUnit(ctx,Object.values(snake_skeleton[i])[0],Object.values(snake_skeleton[i])[1],Object.values(snake_skeleton[i])[3]);
         }
 
     }
@@ -68,4 +65,4 @@ function drawBackground(){
     ctx.fillRect(0,0,myCanvas.width,myCanvas.height)
 }
 
-Snake.drawSnake(Snake.startSnake(7));
+Snake.drawSnake(Snake.startSnake(4));
