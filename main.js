3d34b86ea657f36454e4c549f25a89cf7a25ffb5
Aditya Naik <naik.aditya@gmail.com>
2018-10-25 13:37:21 +0200

    Snake moves successfully after every click

--- main.js
+++ main.js
@@ -15,7 +15,25 @@ class Snake {
 
     }
 
-    static navigate(){
+    static moveSnake(snake_skeleton){
+
+        console.log(snake_skeleton[0]);
+
+        for(let i = 0;i<snake_skeleton.length;i++){
+            //if head, keep direction, else copy direction from previous body part, apply calculcation as per direction
+         let [x,y,direction, head] = snake_skeleton[i];
+           x += direction == 'left' ? 5 : 0;
+           x -= direction =='right' ? 5 : 0;
+           y += direction == "up" ? 5 : 0 ;
+           y -= direction == "down" ? 5:0 ;
+
+           direction =  head ? snake_skeleton[i][2]: snake_skeleton[i-1][2]; 
+
+           snake_skeleton[i] = [x,y,direction, head] ;
+
+        }
+        console.log(snake_skeleton[0]);
+        return snake_skeleton;
 
     }
 
@@ -24,7 +42,7 @@ class Snake {
         let x_position = Math.floor(Math.random()*(parseInt(myCanvas.width,10)/2-101)+100);
         let y_position = Math.floor(Math.random()*(parseInt(myCanvas.height,10)/2-81)+80);
         let init_direction = direction[Math.floor(Math.random()*4)];
-        let snake_skeleton = [{x: x_position,y: y_position,direction:init_direction,head:'Yes'}];
+        let snake_skeleton = [[x_position,y_position,init_direction,true]]; 
 
         for (let i = 0 ; i < length-1 ; i++){
             x_position += init_direction == 'left' ? 5 : 0;
@@ -32,8 +50,7 @@ class Snake {
             y_position += init_direction == "up" ? 5 : 0 ;
             y_position -= init_direction == "down" ? 5:0;
 
-            snake_skeleton.push({x: x_position,y: y_position,direction:init_direction,head:'No'});
-
+            snake_skeleton.push([x_position,y_position,init_direction,false]);
         }
 
         return snake_skeleton;
@@ -43,26 +60,28 @@ class Snake {
 
         for(let i = 0; i< snake_skeleton.length;i++){
 
-            this.drawSnakeUnit(ctx,Object.values(snake_skeleton[i])[0],Object.values(snake_skeleton[i])[1],Object.values(snake_skeleton[i])[3]);
+            this.drawSnakeUnit(ctx,snake_skeleton[i][0],snake_skeleton[i][1],snake_skeleton[i][3]);
         }
 
     }
 
 }
 
-function food(){
-
-
-}
-
-function navigate (){
-
-
-}
 
 function drawBackground(){
     ctx.fillStyle = 'tan';
     ctx.fillRect(0,0,myCanvas.width,myCanvas.height)
 }
 
-Snake.drawSnake(Snake.startSnake(4));
+
+snake = Snake.startSnake(4);
+document.addEventListener('click',function(){
+
+
+    snake = Snake.moveSnake(snake);
+    drawBackground();
+    Snake.drawSnake(snake);
+
+});
+
+Snake.moveSnake(snake);
